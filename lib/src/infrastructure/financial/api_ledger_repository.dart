0ed695f039d409d47ff5c9d1a69148d2de5a8639
Daniel Yudi <danielyudicarvalho@gmail.com>
2026-09-14 15:39:17 -0400

    style(financial): match Dart formatter line width

--- lib/src/infrastructure/financial/api_ledger_repository.dart
+++ lib/src/infrastructure/financial/api_ledger_repository.dart
@@ -189,7 +189,8 @@ final class ApiLedgerRepository implements LedgerRepository {
     balanceCents: row['balanceCents']! as int,
   );
 
-  static String _date(DateTime value) => value.toIso8601String().substring(0, 10);
+  static String _date(DateTime value) =>
+      value.toIso8601String().substring(0, 10);
 }
 
 final class ApiLedgerException implements Exception {
